90b3083829ad43713426e86474342892fd5d9067
Esdras Gomes <esdrasgoomes@gmail.com>
2022-10-08 19:33:45 -0300

    mathematical operators

---
+++ operadores/operadores.dart
@@ -0,0 +1,16 @@
+void main() {
+  double num1 = 20;
+  double num2 = 5;
+
+  double soma = num1 + num2;
+  print(soma);
+
+  double subtrair = num1 - num2;
+  print(subtrair);
+
+  double multiplicar = num1 * num2;
+  print(multiplicar);
+
+  double dividir = num1 / num2;
+  print(dividir);
+}
