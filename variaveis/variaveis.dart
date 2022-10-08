1ee8c8a907dee4c4f34acdd2164b1d1cb9bcf5fa
Esdras Gomes <esdrasgoomes@gmail.com>
2022-10-07 22:44:37 -0300

    Integer type variable

--- variaveis/variaveis.dart
+++ variaveis/variaveis.dart
@@ -1,9 +1,19 @@
-void main() {
-  String nome = "Texto";
-  nome = "Texto2";
-  print(nome);
+// VARIAVEL TIPO STRING
+// void main() {
+//   String nome = "Texto";
+//   nome = "Texto2";
+//   print(nome);
 
-  String cor = "Preto";
-  String resultado = cor ?? "Sem cor";
+//   String cor = "Preto";
+//   String resultado = cor ?? "Sem cor";
+//   print(resultado);
+// }
+
+// VARIAVEL TIPO INTEIRO E DOUBLE
+void main() {
+  int valorUm = 10;
+  int valorDois = 20;
+  int divisor = 2;
+  double resultado = (valorUm + valorDois) / divisor;
   print(resultado);
 }
