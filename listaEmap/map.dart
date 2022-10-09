7fcb4d0a4affd94723f71b1febef27ba5d74e85e
Esdras Gomes <esdrasgoomes@gmail.com>
2022-10-09 13:23:50 -0300

    using map

---
+++ listaEmap/map.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Map usuario = {"nome": "Teste", "idade": "20", "dev": true};
+  // print(usuario);
+  // print(usuario["dev"]); // Mostrando um item específico
+
+  // usuario["idade"] = 25; // Alterando uma informação
+  // print(usuario);
+  // print(usuario.length);
+
+  // Definindo o tipo do Map
+  // Map<int, String> usuario = {1: "Teste", 2: "Maria", 3: "João"};
+  // print(usuario);
+  // print(usuario[3]); // Mostrando um item específico
+
+  // usuario[1] = "Teste2"; // Alterando uma informação
+  // print(usuario);
+  // print(usuario.length);
+
+  // Usando o DYNAMIC
+  Map<int, dynamic> usuario = {1: "Teste", 2: 20, 3: true};
+  print(usuario);
+}
