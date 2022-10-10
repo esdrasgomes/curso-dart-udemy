996b9933d3a75b912682c17f76ef454f66d7cbff
Esdras Gomes <esdrasgoomes@gmail.com>
2022-10-10 20:57:10 -0300

    implementing if and else

---
+++ decisoes e repeticoes/ifElse.dart
@@ -0,0 +1,11 @@
+void main() {
+  var valor = 0;
+
+  if (valor == true) {
+    print("Valor é $valor");
+  } else if (valor == false) {
+    print("Valor é $valor");
+  } else {
+    print("O valor não é verdadeiro nem falso");
+  }
+}
