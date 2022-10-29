1eab3e8c4c2c0154f4754096ec0b5ed4c1d2add6
Esdras Gomes <esdrasgoomes@gmail.com>
2022-10-29 12:01:48 -0300

    adicionando objetos

---
+++ objetos e construtores/objetos.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+class Animal {
+  late String tipo;
+  late String cor;
+  late int idade;
+
+  dormir() {
+    print("O animal $tipo está dormindo.");
+  }
+}
+
+void main() {
+  Animal animal = new Animal();
+  animal.tipo = "Cachorro";
+  animal.cor = "Marron";
+  animal.idade = 5;
+
+  print(animal.tipo);
+  print(animal.cor);
+  print(animal.idade);
+}
